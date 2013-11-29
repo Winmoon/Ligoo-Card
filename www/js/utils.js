87b824e0b977890d018135ef5ef135a7574ee84e
Winmoon 3 <wendell.palazzo@winmoon.com>
2013-11-29 19:03:43 -0200

    bugs,... login facebook

--- www/js/utils.js
+++
@@ -1,4 +0,0 @@
-function parseDate(myDate) {
-	myDate = myDate.split(/[T ]/)[0].replace("-", "").replace("-", "");
-	return myDate.substr(6, 3) + "/" + myDate.substr(4, 2) + "/" + myDate.substr(0, 4);
-}
