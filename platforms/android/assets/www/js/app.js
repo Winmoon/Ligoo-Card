72105afc1c7561e1467ebe831300a31a6f78ac9a
Winmoon 3 <wendell.palazzo@winmoon.com>
2014-01-17 17:41:00 -0200

    Finaliza push no android e inicia no iOS

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -276,14 +276,10 @@ define(['jquery', 'fastclick', 'underscore', 'backbone', 'router', 'mustache' //
 			
 		onNotificationGCM = function(e) {
 				
-				alert("notify");
-				console.log(e);
-				$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
-
 				switch( e.event ) {
 					case 'registered':
 						if (e.regid.length > 0) {
-							$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+							//$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
 							// Your GCM push server needs to know the regID before it can push to this device
 							// here is where you might want to send it the regID for later use.
 							console.log("regID = " + e.regid);
@@ -294,20 +290,14 @@ define(['jquery', 'fastclick', 'underscore', 'backbone', 'router', 'mustache' //
 						// if this flag is set, this notification happened while we were in the foreground.
 						// you might want to play a sound to get the user's attention, throw up a dialog, etc.
 						if (e.foreground) {
-							$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-
-							// if the notification contains a soundname, play it.
 							var my_media = new Media("/android_asset/www/" + e.soundname);
 							my_media.play();
 						} else {// otherwise we were launched because the user touched a notification in the notification tray.
 							if (e.coldstart)
-								$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+								{}
 							else
-								$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+								{}
 						}
-
-						$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-						$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
 						break;
 
 					case 'error':
